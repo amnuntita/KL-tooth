00c3e6f02ee3ef426de0862e99c4a4a9f2385913
KoFeeBriAN <coffeetk2017@gmail.com>
2020-06-23 14:47:39 +0700

    feat(theme): add theme provider

--- lib/themes/app_themes.dart
+++ lib/themes/app_themes.dart
@@ -1,9 +1,12 @@
+import 'default_theme.dart';
 import 'vaccine_theme.dart';
 
 enum AppTheme {
+  DefualtTheme,
   VaccineTheme,
 }
 
 final appThemeData = {
+  AppTheme.DefualtTheme: defaultThemeData,
   AppTheme.VaccineTheme: vaccineThemeData,
 };
